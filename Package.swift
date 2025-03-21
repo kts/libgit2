1a86aa6f58af0c37ddf7addba58540436027e3a2
Ken Schutte <kenschutte@gmail.com>
2025-03-21 11:20:41 -0600

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+  name: "libgit2",
+  products: [
+    .library(
+      name: "libgit2",
+      targets: ["libgit2"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "libgit2",
+      path: ".",
+    )
+  ])
